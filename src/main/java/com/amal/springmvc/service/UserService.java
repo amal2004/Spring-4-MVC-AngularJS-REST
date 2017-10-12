48c87c11c1aa732ad30e55de43ed177e0a579735
Amal <Amal@Amal-PC>
2017-10-12 18:02:18 -0400

    Completed project

---
+++ src/main/java/com/amal/springmvc/service/UserService.java
@@ -0,0 +1,24 @@
+package com.amal.springmvc.service;
+
+import java.util.List;
+
+import com.amal.springmvc.model.User;
+
+public interface UserService {
+
+	User findById(long id);
+
+	User findByName(String name);
+
+	void saveUser(User user);
+
+	void updateUser(User user);
+
+	void deleteUserById(long id);
+
+	List<User> findAllUsers();
+
+	void deleteAllUsers();
+
+	public boolean isUserExist(User user);
+}
